064252f7e9930ad1b24a8db10cc127bba264d031
Kavon Farvardin <kavon@farvard.in>
2018-01-20 16:23:37 -0600

    looks good

--- README.md
+++ README.md
@@ -3,6 +3,16 @@ visualize an AST serialized as YAML
 
 ### Requirements
 
+You need PyYAML and GraphViz installed.
+
 ```
 sudo pip install pyyaml
+brew install graphviz
+```
+
+### Usage
+
+```
+python show.py ast.yaml > graph.gv
+dot -Tpdf graph.gv -o render.pdf
 ```
--- show.py
+++ show.py
@@ -3,6 +3,65 @@
 import sys
 import yaml
 
+
+def out (s):
+    sys.stdout.write(s)
+
+uniq = 1
+
+def getID():
+    global uniq
+    n = str(uniq)
+    uniq += 1
+    return n
+    
+def asAttr(attr, val):
+    return " [" + attr + "=\"" + val + "\"]"
+
+def render(ast, name=None):
+    if name is None:
+        name = getID()
+        
+    lab = ""
+    edges = []  # (from, to, attr)
+    
+    if type(ast) is dict:
+        for key,val in ast.iteritems():
+            
+            if type(val) is dict:
+                child = getID()
+                edges.append((name, child, asAttr("label", key)))
+                render(val, child)
+                
+            elif type(val) is list:
+                last = None
+                for i in val:
+                    child = getID()
+                    edges.append((name, child, asAttr("label", key)))
+                    render(i, child)
+                    if last is not None:
+                        edges.append((last, child, asAttr("style", "dashed")))
+                    last = child
+                
+            else:
+                lab += (str(key) + ": " + str(val) + "\\n")
+                
+    else:
+        lab += str(ast)
+    
+    # output vertex
+    out(name + asAttr("label", lab) + ";\n")
+    
+    # output edges
+    for (src, dest, attr) in edges:
+        out(src + " -> " + dest + attr + ";\n")
+    
+
+
+
+######################
+# "main"
+
 # check args
 myself = sys.argv[0]
 if len(sys.argv) != 2:
@@ -12,9 +71,16 @@ if len(sys.argv) != 2:
 
 infile = sys.argv[1]
 
-
 with open(infile, 'r') as stream:
     try:
-        print(yaml.load(stream))
+        ast = yaml.load(stream)
+
+        out ("digraph graphname {\n")
+        
+        render(ast)
+        
+        out ("}\n")
+        
     except yaml.YAMLError as exc:
         print(exc)
+        sys.exit(1)
